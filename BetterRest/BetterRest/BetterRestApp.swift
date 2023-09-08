0fa013eee85c88222b095f0aeca94379f59eb95a
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-08 13:42:05 +0900

    init commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Анна Перехрест  on 2023/09/08.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
