0ef0dbb6fee5324f652d7f45ede206a55e700515
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-08 15:17:28 +0900

    Create UI

--- BetterRest/BetterRest/BetterRestApp.swift
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct BetterRestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(gradient: Gradient(colors: [
+                Color(.init(red: 0.8, green: 0.8, blue: 0.4, alpha: 0.6)),
+                Color(.init(red: 0.4, green: 0.3, blue: 0.8, alpha: 0.6))
+            ]), center: .center, startRadius: 50, endRadius: 600)
+                .ignoresSafeArea()
+
+            VStack {
+                VStack(spacing: 30) {
+                                Text("When do you want to wake up?")
+                                    .font(.headline)
+                                
+                                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                                    .labelsHidden()
+                                
+                                Text("Desired amount of sleep")
+                                    .font(.headline)
+                                
+                                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                                
+                                Text("Daily coffee intake")
+                                    .font(.headline)
+                                
+                                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                            }
+                            .padding()
+                            .background(.thinMaterial)
+                            .cornerRadius(30)
+                            .padding()
+                
+                Button {
+                    calculateBedtime()
+                } label: {
+                    Text("Calculate")
+                        .padding()
+                        .foregroundColor(.primary)
+                        .background(.thinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+                }
+            }
+            .navigationTitle("BetterRest")
         }
-        .padding()
+    }
+    
+    func calculateBedtime() {
     }
 }
 
